d9580f2e07743ee62ec4e05467a7e56c99093cc9
Yuva <yuva@codemancers.com>
2015-09-10 19:45:19 +0530

    use guards to make things even more simpler!

--- haskell/exercise-01-10.hs
+++ haskell/exercise-01-10.hs
@@ -1,8 +1,9 @@
-ackermann x 0 = 0
-ackermann 0 y = 2 * y
-ackermann x 1 = 2
-ackermann x y =
-  ackermann (x - 1) (ackermann x (y - 1))
+ackermann :: Int -> Int -> Int
+ackermann x y
+  | y == 0 = 0
+  | x == 0 = 2 * y
+  | y == 1 = 2
+  | otherwise = ackermann (x - 1) (ackermann x (y - 1))
 
 -- ackermann 0 n = 2 * n
 -- ackermann 1 n = 2^n
