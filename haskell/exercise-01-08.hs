1a35e5cf816d69f7723d8dc6e2a6284b5f3075b9
Yuva <yuva@codemancers.com>
2015-09-10 20:20:18 +0530

    improve cubeRoot by defining methods using let

--- haskell/exercise-01-08.hs
+++ haskell/exercise-01-08.hs
@@ -1,10 +1,9 @@
-improve guess x = (x/(guess^2) + 2 * guess)/3
-goodEnough guess x = (abs (guess^3 - x)) < 0.001
-
-cubeIter guess x =
-  if goodEnough guess x
-  then guess
-  else cubeIter (improve guess x) (x)
-
-
-cubeRoot x = cubeIter 1.0 x
+cubeRoot :: Double -> Double
+cubeRoot x =
+  let improve guess = (x/(guess^2) + 2 * guess)/3
+      goodEnough guess = (abs (guess^3 - x)) < 0.001
+      cubeIter guess =
+        if goodEnough guess
+        then guess
+        else cubeIter (improve guess)
+  in cubeIter 1.0
