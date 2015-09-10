ed7a2240c6ffa07f1e0d0b44713802e9a8ff1931
Yuva <yuva@codemancers.com>
2015-09-10 19:19:14 +0530

    use pattern matching instead of nested if-elses

--- haskell/exercise-01-10.hs
+++ haskell/exercise-01-10.hs
@@ -1,11 +1,8 @@
+ackermann x 0 = 0
+ackermann 0 y = 2 * y
+ackermann x 1 = 2
 ackermann x y =
-  if y == 0
-  then 0
-  else if x == 0
-       then 2 * y
-       else if y == 1
-            then 2
-            else ackermann (x - 1) (ackermann x (y - 1))
+  ackermann (x - 1) (ackermann x (y - 1))
 
 -- ackermann 0 n = 2 * n
 -- ackermann 1 n = 2^n
