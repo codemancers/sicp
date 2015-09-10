0a84a46a827db5af90052e0c8858a351d5c8f28d
goromlagche <mrinmoy.das91@gmail.com>
2015-09-10 19:54:24 +0530

    solution to 1.11

--- clojure/ex_1_2.clj
+++ clojure/ex_1_2.clj
@@ -1,6 +1,5 @@
 (ns sicp.ch.1.2)
 
-
 ;; Exercise 1.10.  The following procedure computes a mathematical function
 ;; called akermann's function.
 
@@ -44,3 +43,54 @@
 (defn five_n_square
   [k n]
   (* 5 n n))
+
+
+;; hidden problem
+;; make cc_recur iterative process
+;; will do later seems pretty difficult
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+
+(defn cc_recur
+  [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc_recur amount
+                     (- kinds-of-coins 1))
+                 (cc_recur (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins))))
+
+(defn count_change
+  [amount]
+  (cc_recur amount 5))
+
+;; Exercise 1.11.  A function f is defined by the rule that
+;; f(n) = n if n<3
+;; f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3
+;; Write a procedure that computes f by means of a recursive process.
+;; Write a procedure that computes f by means of an iterative process.
+
+(defn ex_1_11_recur
+  [n]
+  (if (< n 3)
+    n
+    (+ (ex_1_11_recur (- n 1))
+       (* 2 (ex_1_11_recur (- n 2)))
+       (* 3 (ex_1_11_recur (- n 3))))))
+
+(defn ex_1_11_iter [n]
+  (letfn [(ex_1_11_help
+            [n1 n2 n3 cnt]
+            (def val (+ n1 (* 2 n2) (* 3 n3)))
+            (if (= cnt n)
+              val
+              (ex_1_11_help val n1 n2 (inc cnt))))]
+    (if (< n 3) n
+        (ex_1_11_help 2 1 0 3))))
