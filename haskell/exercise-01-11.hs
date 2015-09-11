d5606270f1557d1ce902946b2690d658b8bc15b9
Yuva <yuva@codemancers.com>
2015-09-11 19:43:54 +0530

    improve code using tuples

--- haskell/exercise-01-11.hs
+++ haskell/exercise-01-11.hs
@@ -4,15 +4,12 @@ fRecursive n
   | otherwise = fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3)
 
 
-fIter acc n
-  | n == 2 = head acc
+fIter (a, b, c) n
+  | n == 2 = a
   | otherwise =
-      let im1 = head acc
-          im2 = head (tail acc)
-          im3 = head (tail (tail acc))
-          val = im1 + 2 * im2 + 3 * im3
-      in fIter (val:acc) (n - 1)
+      let val = a + 2 * b + 3 * c
+      in fIter (val, a, b) (n - 1)
 
 fIterative n
   | n < 3 = n
-  | otherwise = fIter [ 2, 1, 0 ] n
+  | otherwise = fIter (2, 1, 0) n
