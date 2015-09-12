b1db36d3432d3325d1dd485faa06ee20da6956f6
Yuva <yuva@codemancers.com>
2015-09-12 12:07:22 +0530

    add solution for pascal triange ex 1.12

---
+++ haskell/exercise-01-12.hs
@@ -0,0 +1,8 @@
+sum2Lists [x] [] = x:[]
+sum2Lists [x] [y] = (x+y):[]
+sum2Lists (x:xs) (y:ys) = (x+y):sum2Lists xs ys
+
+pascalTriangle 1 = [1]
+pascalTriangle n =
+  let list = pascalTriangle (n - 1)
+  in sum2Lists (0:list) list
