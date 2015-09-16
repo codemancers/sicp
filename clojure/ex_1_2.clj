2035eb32e655d948b8f5c6d1389db21de62504d0
goromlagche <mrinmoy.das91@gmail.com>
2015-09-16 19:54:39 +0530

    solution to 1.16
    This is a pretty cool problem, explains about invariant quantity

--- clojure/ex_1_2.clj
+++ clojure/ex_1_2.clj
@@ -168,3 +168,23 @@
 ;; space complexity :
 ;; height of the tree = number of nodes
 ;; O(log n)
+
+
+;; Exercise 1.16.  Design a procedure that evolves an iterative exponentiation process
+;; that uses successive squaring and uses a logarithmic number of steps, as does fast-expt.
+;; (Hint: Using the observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and
+;;        the base b, an additional state variable a, and define the state transformation
+;;        in such a way that the product a bn is unchanged from state to state. At the
+;;        beginning of the process a is taken to be 1, and the answer is given by the value
+;;        of a at the end of the process. In general, the technique of defining an invariant
+;;        quantity that remains unchanged from state to state is a powerful way to think about
+;;        the design of iterative algorithms.)
+
+(defn fast-expt-iter
+  [b n]
+  (letfn[(expt-iter
+           [ b n a ]
+           (cond (= n 0) a
+                 (even? n) (expt-iter (* b b) (/ n 2) a)
+                 :else (expt-iter b (- n 1) (* a b))))]
+    (expt-iter b n 1)))
