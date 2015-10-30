dde3c550279ffb3a96dd78ed453fd21bc258e1c6
goromlagche <mrinmoy.das91@gmail.com>
2015-10-30 19:07:29 +0530

    add ex-1.21

--- clojure/ex_1_2.clj
+++ clojure/ex_1_2.clj
@@ -271,3 +271,29 @@
                                   q
                                   (- count 1))))]
     (fib-iter 1 0 0 1 n)))
+
+
+;; Exercise 1.21.  Use the smallest-divisor procedure to find the smallest divisor of each
+;; of the following numbers: 199, 1999, 19999.
+
+(defn smallest-divisor
+  [n]
+  (letfn [(divides?
+            [a b]
+            (= (mod b a) 0))
+          (square
+            [x]
+            (* x x))
+          (get-divisor
+            [n test-divisor]
+            (cond (> (square test-divisor) n) n
+                  (divides? test-divisor n) test-divisor
+                  :else (recur n (+ test-divisor 1))))]
+    (get-divisor n 2)))
+
+(smallest-divisor 199)
+; 199
+(smallest-divisor 1999)
+; 1999
+(smallest-divisor 19999)
+; 7
